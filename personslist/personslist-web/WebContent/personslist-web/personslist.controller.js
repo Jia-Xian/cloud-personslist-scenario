c8f68e4461e73c65e629facce2d3396d7f8fa1c4
Jens Glander <jens.glander@sap.com>
2013-06-21 06:41:23 +0200

    [v-1.0] Section 2.3 of PersonsList Cloud Tutorial
    Section 2.3: Extend Web Application UI - PersonsList Table

--- personslist/personslist-web/WebContent/personslist-web/personslist.controller.js
+++ personslist/personslist-web/WebContent/personslist-web/personslist.controller.js
@@ -1,13 +1,26 @@
 sap.ui.controller("personslist-web.personslist", {
 
-
-/**
-* Called when a controller is instantiated and its View controls (if available) are already created.
-* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-*/
-//   onInit: function() {
-//
-//   },
+	onInit : function() {
+		var oPersonsModel = new sap.ui.model.json.JSONModel();
+		oPersonsModel.setData({
+			Persons : [ {
+				FirstName : "",
+				LastName : ""
+			} ]
+		});
+		this.getView().setModel(oPersonsModel);
+	},
+	addNewPerson : function(sFirstName, sLastName, oTable) {
+		var oPersonsModel = new sap.ui.model.json.JSONModel();
+		oPersonsModel.setData({
+			Persons : [ {
+				FirstName : sFirstName,
+				LastName : sLastName
+			} ]
+		});
+		this.getView().setModel(oPersonsModel);
+		oTable.unbindRows().bindRows("/Persons");
+	},
 
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
