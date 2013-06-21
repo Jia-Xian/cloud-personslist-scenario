9720e04fbb27e60d763bd62d8bd0ac89e6940951
Jens Glander <jens.glander@sap.com>
2013-06-21 06:36:50 +0200

    [v-1.0] Section 2.1 of PersonsList Cloud Tutorial
    Section 2.1: Create Web Project for SAPUI5 Application Development

---
+++ personslist/personslist-web/WebContent/personslist-web/personslist.view.js
@@ -0,0 +1,11 @@
+sap.ui.jsview("personslist-web.personslist", {
+
+      getControllerName : function() {
+         return "personslist-web.personslist";
+      },
+
+      createContent : function(oController) {
+
+      }
+
+});
